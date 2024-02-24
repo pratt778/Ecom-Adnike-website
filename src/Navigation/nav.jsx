367d8e5b1b9814350ec6bf8a6237f95baf83e3ea
Pratham Sharma <pratham016914934@gmail.com>
2024-02-24 22:30:17 +0545

    minor updates

---
+++ src/Navigation/nav.jsx
@@ -0,0 +1,12 @@
+import { AiFillHeart } from "react-icons/ai";
+import { AiOutlineShoppingCart, AiOutlineUserAdd } from "react-icons/ai";
+const Nav = ()=>{
+    return <>
+    
+    
+    
+    </>
+}
+
+
+export default Nav;
