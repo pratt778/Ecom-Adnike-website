c5b639ab1a0cbe9d1de31502ea1c6469b0d3dcf3
Pratham Sharma <pratham016914934@gmail.com>
2024-02-28 22:00:18 +0545

    added some features on categories

--- src/sidebar/category.jsx
+++ src/sidebar/category.jsx
@@ -3,26 +3,27 @@ const Category = ()=>{
     <div className="category">
         <h2 className="text-xl font-bold">Category</h2>
         <div className="categories flex flex-col gap-1 mt-2">
-            <label htmlFor="all" className="flex gap-1">
-                <input type="radio" id="all" name="btn_rd" />
-                <span className="font-semibold">All</span>
+            <label htmlFor="all" className="flex gap-1 list-cont select-none relative pl-10">
+                <input type="radio" id="all" name="btn_rd" className="absolute cursor-pointer opacity-0" />
+                <span className="font-semibold checkmark absolute rounded-full top-0 left-0 bg-[#eee] h-[20px] w-[20px]"></span>All
             </label>
-            <label htmlFor="snk" className="flex gap-1 ">
-                <input type="radio" id="snk" name="btn_rd" />
-                <span className="font-semibold">Sneakers</span>
+            <label htmlFor="sn" className="flex gap-1 list-cont select-none relative pl-10">
+                <input type="radio" id="sn" name="btn_rd" className="absolute cursor-pointer opacity-0" />
+                <span className="font-semibold checkmark absolute rounded-full top-0 left-0 bg-[#eee] h-[20px] w-[20px]"></span>Sneakers
             </label>
-            <label htmlFor="ft" className="flex gap-1">
-                <input type="radio" id="ft" name="btn_rd" />
-                <span className="font-semibold">Flats</span>
+            <label htmlFor="sd" className="flex gap-1 list-cont select-none relative pl-10">
+                <input type="radio" id="sd" name="btn_rd" className="absolute cursor-pointer opacity-0" />
+                <span className="font-semibold checkmark absolute rounded-full top-0 left-0 bg-[#eee] h-[20px] w-[20px]"></span>Sandals
             </label>
-            <label htmlFor="hl" className="flex gap-1">
-                <input type="radio" id="hl" name="btn_rd" />
-                <span className="font-semibold">Heels</span>
+            <label htmlFor="hl" className="flex gap-1 list-cont select-none relative pl-10">
+                <input type="radio" id="hl" name="btn_rd" className="absolute cursor-pointer opacity-0" />
+                <span className="font-semibold checkmark absolute rounded-full top-0 left-0 bg-[#eee] h-[20px] w-[20px]"></span>Heels
             </label>
-            <label htmlFor="sd" className="flex gap-1">
-                <input type="radio" id="sd" name="btn_rd" />
-                <span className="font-semibold">Sandals</span>
+            <label htmlFor="ft" className="flex gap-1 list-cont select-none relative pl-10">
+                <input type="radio" id="ft" name="btn_rd" className="absolute cursor-pointer opacity-0" />
+                <span className="font-semibold checkmark absolute rounded-full top-0 left-0 bg-[#eee] h-[20px] w-[20px]"></span>Flats
             </label>
+           
             
         </div>
     </div>
